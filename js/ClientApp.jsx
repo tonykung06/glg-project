39f2e1b46fd10fe776b9f895be3228ca8bdc6aa6
tonykung <tonykung06@gmail.com>
2016-04-16 19:21:13 +0800

    add react-toolbox and make homepage

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,13 +1,12 @@
+require('../scss/style.scss');
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Landing from './Landing.jsx';
-import Search from './Search.jsx';
 import {Router, Route, hashHistory} from 'react-router';
 
 const App = () => (
   <Router history={hashHistory}>
     <Route path="/" component={Landing} />
-    <Route path="/search" component={Search} />
   </Router>
 );
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+const webpack = require('webpack');
 
 module.exports = {
   context: __dirname,
@@ -7,8 +10,9 @@ module.exports = {
     path: path.join(__dirname, '/public'),
     filename: 'bundle.js'
   },
+  devtool: 'inline-source-map',
   resolve: {
-    extensions: ['', '.js', '.jsx', '.json']
+    extensions: ['', '.js', '.jsx', '.json', '.scss']
   },
   stats: {
     colors: true,
@@ -16,17 +20,30 @@ module.exports = {
     chunks: false
   },
   module: {
-    preLoaders: [{
-      test: /\.jsx?$/,
-      loader: 'eslint-loader',
-      exclude: /node_modules/
-    }],
+    // preLoaders: [{
+    //   test: /\.jsx?$/,
+    //   loader: 'eslint-loader',
+    //   exclude: /node_modules/
+    // }],
     loaders: [{
       test: /\.jsx?$/,
       loader: 'babel-loader'
     }, {
       test: /\.json$/,
       loader: 'json-loader'
+    }, {
+      test: /(\.s?css)$/,
+      loader: ExtractTextPlugin.extract('style', 'css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass?sourceMap!toolbox')
     }]
-  }
+  },
+  toolbox: {
+    theme: path.join(__dirname, 'scss/toolbox-theme.scss')
+  },
+  postcss: [autoprefixer],
+  plugins: [
+    new webpack.NoErrorsPlugin(),
+    new ExtractTextPlugin('react-toolbox.css', {
+      allChunks: true
+    })
+  ]
 };
